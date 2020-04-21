2fb0d8ed16b51bc4ed69e11916c4f4d89079b46c
Aleksandr Kisliy <akisliy2306#gmail.com>
2020-04-21 19:59:41 +0300

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './Users/users.module';
 import { BooksModule } from './Books/books.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGOLAB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     }),
     UsersModule,
     BooksModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
